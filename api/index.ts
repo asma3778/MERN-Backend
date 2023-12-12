19842492dfae17368f09428af1158932061c753e
asma3778 <asmasaad3778@gmail.com>
2023-12-12 15:16:52 +0300

    update

--- api/index.ts
+++ api/index.ts
@@ -26,7 +26,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Health checkup' })
+  res.status(200).json('Backend project')
 })
 
 app.use('/products', productRoute)
