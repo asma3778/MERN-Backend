60fc26b6dd2e72da098ca5b863c66dc4164b451d
asma3778 <asmasaad3778@gmail.com>
2023-12-12 12:54:28 +0300

    update

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,24 +1,25 @@
 import cookieParser from 'cookie-parser'
 import express, { Application, Request, Response } from 'express'
 
-import { dev } from './config'
-import { connectDB } from './config/db'
-import { errorHandler } from './middlewares/errorHandler'
-import authRoute from './routers/authRoute'
-import categoriesRouter from './routers/categoryRoute'
-import ordersRouter from './routers/orderRoute'
-import productRoute from './routers/productRoute'
-import usersRouter from './routers/userRoute'
-import { createHttpError } from './util/createHTTPError'
+import { dev } from '../config'
+import { connectDB } from '../config/db'
+import { errorHandler } from '../middlewares/errorHandler'
+import authRoute from '../routers/authRoute'
+import categoriesRouter from '../routers/categoryRoute'
+import ordersRouter from '../routers/orderRoute'
+import productRoute from '../routers/productRoute'
+import usersRouter from '../routers/userRoute'
+import { createHttpError } from '../util/createHTTPError'
 
 const app: Application = express()
 
-const port: number = dev.app.port || 3003
+// const port: number = dev.app.port || 3003
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`)
+// })
   connectDB()
-})
+
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
